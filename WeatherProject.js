64b8299cde1ad5e58105ccdacb9c0901a69290d3
RainGorr <anv7@sina.com>
2019-11-10 17:41:50 +0800

    React-native组件小试

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,53 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
-import React, { Component } from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+class WeatherProject extends Component{
+    constructor(props){
+        super(props)
+        this.state = {zip:''}
+    }
 
-export default class MyApp extends Component {
-    render() {
+    _handleTextChange = event => {
+        this.setState({
+            zip: event.nativeEvent.text
+        })
+    }
+
+    render(){
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native❓
+            <view>
+                <Text>
+                    You input { this.state.zip }
                 </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
-            </View>
-        );
+                <TextInput onSubmitEditing={this._handleTextChange} />
+            </view>
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
 
-AppRegistry.registerComponent('MyApp', () => MyApp);
+export default WeatherProject
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,6 @@
 import {AppRegistry} from "react-native"
 import WeatherProject from "./WeatherProject"
 AppRegistry.registerComponent("WeatherProject", () => WeatherProject)
+
+
+
